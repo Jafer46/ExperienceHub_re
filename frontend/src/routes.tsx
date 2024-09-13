ef780e9cd11aad9bc769f3734bf96bbc4859143c
Jafer46 <123theelite@gmail.com>
2024-09-13 12:32:26 -0700

    adding shadcn and other components

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from 'react-router-dom'
 import Layout from './pages/user/Layout'
 import AdminLayout from './pages/Admin/Layout'
+import Dashboard from './pages/Admin/Dashboard'
+import UserManagement from './pages/Admin/UserManagement'
 
 export const routes = createBrowserRouter([
   {
@@ -11,6 +13,9 @@ export const routes = createBrowserRouter([
   {
     path: '/admin',
     element: <AdminLayout />,
-    children: []
+    children: [
+      { index: true, element: <Dashboard /> },
+      { path: '/users', element: <UserManagement /> }
+    ]
   }
 ])
